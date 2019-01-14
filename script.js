dae492b8550217d1a7a063ac349e6774941373c4
DavidHamelin <hamelindavid@hotmail.fr>
2019-01-14 15:59:16 +0100

    correction

---
+++ script.js
@@ -0,0 +1,9 @@
+﻿function mouseOver()
+{
+    // correction :
+    document.getElementById("image1").src = 'images/image1_2.jpg';
+
+    /*var switchImg = document.getElementById("image1").innerHTML;
+    switchImg = "<img src='images/image1_2.jpg'/>";
+    document.write(switchImg);*/
+}
